53d8cb0d351e3848e0b8bc0bf7ee4f33185a8373
Adam Bonnet <abo@open-net.ch>
2024-02-28 19:08:03 +0100

    chore(format): Run formatting

--- co2/cli/cli.py
+++ co2/cli/cli.py
@@ -1,14 +1,12 @@
-import typer
-
 import importlib
 import pkgutil
 
+import typer
 
 discovered_plugins = {
     name: importlib.import_module(name)
-    for finder, name, ispkg
-    in pkgutil.iter_modules()
-    if name.startswith('co2_')
+    for finder, name, ispkg in pkgutil.iter_modules()
+    if name.startswith("co2_")
 }
 
 cli = typer.Typer(no_args_is_help=True)
@@ -16,10 +14,12 @@ cli = typer.Typer(no_args_is_help=True)
 for name, plugin in discovered_plugins.items():
     cli.add_typer(plugin.cli, name=name.replace("co2_", ""))
 
+
 @cli.command()
 def hello():
     print("Hello")
-    
+
+
 @cli.command()
 def goodbye():
     print("Goodbye")
